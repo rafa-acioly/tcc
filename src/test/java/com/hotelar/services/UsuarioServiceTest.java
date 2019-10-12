c08418d9450045923d16b196f2f7eb6b79f32017
Rafael Acioly <rafael.acioly@luizalabs.com>
2019-10-11 23:34:59 -0300

    adiciona teste para usuario service

---
+++ src/test/java/com/hotelar/services/UsuarioServiceTest.java
@@ -0,0 +1,20 @@
+package com.hotelar.services;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class UsuarioServiceTest {
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+}
