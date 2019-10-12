9ffa6bea669d84fd4dffe5eb688951a25cab2d28
Rafael Acioly <rafael.acioly@luizalabs.com>
2019-10-11 23:30:00 -0300

    cria classe de conexao com o postgres

--- src/main/java/com/hotelar/entidades/BancoDeDados.java
+++ src/main/java/com/hotelar/entidades/BancoDeDados.java
@@ -1,6 +1,35 @@
 package com.hotelar.entidades;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+
 
 public class BancoDeDados {
     
+    @Produces
+    @RequestScoped
+    public static Connection getConexao(){
+        try {
+            Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "postgres"); //retorna um Connection
+        } catch (ClassNotFoundException | SQLException e) {
+            Logger.getLogger(BancoDeDados.class.getName()).log(Level.SEVERE, null, e);
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public void fechaConexao(@Disposes Connection conexao) {
+        try {
+            conexao.close();
+        } catch (SQLException e) {
+            Logger.getLogger(BancoDeDados.class.getName()).log(Level.SEVERE, null, e);
+            throw new RuntimeException(e);
+        }
+    }
 }
